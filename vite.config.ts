34707fe31ef1b53b71f9270fc5851cc0ebe76128
tsifcarnage <59823010+tsifcarnage@users.noreply.github.com>
2026-06-25 12:30:39 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,9 +8,6 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), tailwindcss()],
-  server: {
-    host: true,
-  },
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
